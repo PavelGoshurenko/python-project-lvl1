1f59c4f190037777acae6d4a71c74251352950c3
Pavel Goshurenko <goshurenkopl@gmail.com>
2020-02-20 08:57:16 +0200

    import error

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,10 @@
+from brain_games import cli
+from brain_games.cli import welcome_user
+
 def main():
     print("Welcome to the Brain Games!")
+    cli.welcome_user()
+
 
 if __name__ == '__main__':
     main()
